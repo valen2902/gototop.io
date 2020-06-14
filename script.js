97008498218a23ade82d29ab1b600e7ab8b34854
Valentina Buza <58169886+valen2902@users.noreply.github.com>
2020-06-14 23:47:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+let sliderEl = $('.main-inner');
+let sliderBox = $('.slider');
+
+let nowShowSlide = 0;
+
+let sliderItems = sliderEl.find('.index-main, .news-1-main, .news-2-main, .contacts-main');
+let firstSlide = sliderItems.eq(0).clone();
+sliderBox.append(firstSlide);
+
+sliderItems = sliderEl.find('.index-main, .news-1-main, .news-2-main, .contacts-main');
+
+let counter = 0;
+sliderEl.find('.right').click(function(){
+    counter++;
+
+    if (sliderItems.length - 1 > counter ) {
+        
+        sliderBox.animate( {
+            'left' : -100 * counter + '%'
+        }, 1000);
+    } else {
+        
+        sliderBox.animate( {
+            'left' : -100 * counter + '%'
+        }, 1000, function() {
+            sliderBox.css('left', 0);
+            
+        });
+        counter = 0;
+    }
+});
+
+
+sliderEl.find('.left').click(function(){
+    if (counter == 0) {
+        sliderBox.animate( {
+            'left' : -100 * (sliderItems.length - 1) + '%'
+        }, 0, function() {
+            
+            sliderBox.animate( {
+            'left' : -100 * (sliderItems.length - 2) + '%'
+            }, 1000)
+
+        });
+        counter = sliderItems.length - 2;
+    } else {
+        counter--;
+        sliderBox.animate( {
+            'left' : -100 * counter + '%'
+        }, 1000);
+    }
+});
+
+
+$('.slider .center-box .btn').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('.background-cards').offset().top
+    }, 1000);
+});
+
+let formsEl = $('.forms-inner, .forms-inner-contacts');
+let popupEl = $('.popup');
+$('.btn-call').click(function(e){
+    e.preventDefault(); 
+    popupEl.css('display', 'flex').hide().fadeIn(500);
+});
+
+$('.popup-inner-close').click(function(){
+    popupEl.fadeOut(500);
+    popupEl.find('.popup-inner-success').delay(500).fadeOut();
+    popupEl.find('h3').delay(500).slideDown();
+    popupEl.find('form').delay(500).slideDown();
+    popupEl.find('input, textarea').val('');
+});
+$('form').submit(function(e){
+    e.preventDefault();
+
+    let fioEl = $(this).find("[name='fio']");
+    let emailEl = $(this).find("[name='email']");
+    let telEl = $(this).find("[name='phone']");
+
+    if( fioEl.val() != '' && emailEl.val() != '' && telEl.val() != '' ){
+        fioEl.removeClass('error');
+        emailEl.removeClass('error');
+        telEl.removeClass('error');
+
+        popupEl.find('h3').delay(500).slideUp(1000);
+        popupEl.find('form').slideUp(1000);
+
+        popupEl.find('.popup-inner-success')
+            .html('<b><u>' + fioEl.val() + '</u></b>, менеджер вам позвонит по номеру <b><u>' + telEl.val() + '</u></b>')
+            .delay(1500)
+            .fadeIn();
+        // this.submit();
+        formsEl.find('h3').delay(500).slideUp(1000);
+        formsEl.find('form').slideUp(1000);
+
+        formsEl.find('.forms-inner-success, .forms-inner-contacts-success')
+            .html('<b><u>' + fioEl.val() + '</u></b>, менеджер вам позвонит по номеру <b><u>' + telEl.val() + '</u></b>')
+            .delay(1500)
+            .fadeIn();
+        // // this.submit();
+        formsEl.find('.forms-inner-contacts-success')
+            .html('<b><u>' + fioEl.val() + '</u></b>, менеджер напишет вам на почту <b><u>' + emailEl.val() + '</u></b>')
+            .delay(1500)
+            .fadeIn();
+    }else{
+        if( fioEl.val() == '' ){
+            fioEl.addClass('error');
+        }else{
+            fioEl.removeClass('error');    
+        }
+
+        if( emailEl.val() == '' ){
+            emailEl.addClass('error');
+        }else{
+            emailEl.removeClass('error');    
+        }
+
+        if( telEl.val() == '' ){
+            telEl.addClass('error');
+        }else{
+            telEl.removeClass('error');    
+        }
+    }
+
+});
+$('input').keyup(function(e){
+    if( e.keyCode != 9 && e.keyCode != 27 ){
+        $(this).removeClass('error');
+    }
+});
+
+$(window).keyup(function(e){
+    if( e.keyCode == 27 ){
+        popupEl.fadeOut();   
+    }
+});
+
+let navEl = $('header nav');
+let burgerEl = $('header .burger');
+
+burgerEl.click(function(){
+    navEl.toggleClass('open');
+    burgerEl.toggleClass('open'); 
+});
+
+$('.cards-item').click(function(e){
+    e.preventDefault();
+    $(this).find('.cards-item-text').slideToggle();
+});
